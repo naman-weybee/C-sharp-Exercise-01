d16aebb05ad011e3e3afb443f5e20bf7673880a4
naman-weybee <naman@weybee.in>
2022-08-02 15:02:23 +0530

    performed exercise 01 in C sharp

---
+++ Exercise-01/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+
+namespace Exercise_01
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Enter Number Line...!");
+            int num = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("----- Half Pyramid -----");
+            Console.WriteLine();
+            char k;
+
+            for (int i = 1; i <= num; i++)
+            {
+                k = 'A';
+                for (int j = 1; j <= i; j++)
+                {
+                    Console.Write(k++ + " ");
+                }
+                k--;
+                for (int m = 1; m < i; m++)
+                {
+                    Console.Write(--k + " ");
+                }
+                Console.WriteLine();
+            }
+            Console.ReadLine();
+        }
+
+        //public static void Main(string[] args)
+        //{
+        //    Console.WriteLine("Enter Number Line...!");
+        //    int num = Convert.ToInt32(Console.ReadLine());
+        //    Console.WriteLine("----- Full Pyramid -----");
+
+        //    Console.WriteLine();
+        //    char k;
+
+        //    for (int i = 1; i <= num; i++)
+        //    {
+        //        k = 'A';
+        //        for (int n = num; n > i; n--)
+        //        {
+        //            Console.Write(" ");
+        //        }
+        //        for (int j = 1; j <= i; j++)
+        //        {
+        //            Console.Write(k++);
+        //        }
+        //        k--;
+        //        for (int m = 1; m < i; m++)
+        //        {
+        //            Console.Write(--k);
+        //        }
+        //        Console.WriteLine();
+        //    }
+        //    Console.ReadLine();
+        //}
+    }
+}
